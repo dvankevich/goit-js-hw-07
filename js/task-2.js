be23c8b6fda1734316502f0a649ef184bbf979e1
dvankevich <dvankevich@gmail.com>
2024-07-18 20:25:20 +0300

    task 2 wrote the script

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +24,37 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+// Напиши скрипт для створення галереї зображень на основі масиву даних.
+// HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+
+// Ти можеш створити й додати HTML - елементи, використовуючи
+// document.createElement() і elem.append()
+// або шаблонні рядки і elem.insertAdjacentHTML().
+
+//   Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+//   Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+// console.log(images);
+const gallery = document.querySelector(".gallery");
+// console.log(gallery);
+
+// console.log(images.length);
+
+let galleryItems = "";
+
+for (let index = 0; index < images.length; index++) {
+  const element = images[index];
+  // console.log(`<li><img src="${element.url}" alt="${element.alt}"</li>`);
+  galleryItems += `<li><img src="${element.url}" alt="${element.alt}"</li>`;
+}
+
+// console.log(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", galleryItems);
