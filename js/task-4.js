8195f66122ce33e1c703ecf5b05e1167a5bf002e
dvankevich <dvankevich@gmail.com>
2024-07-19 19:36:57 +0300

    task 4 wrote the script

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+"use strict";
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim(); // хоча для паролів можна і пробіли використовувати
+
+  if (email === "" || password === "") {
+    //  https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+    //return console.log("Please fill in all the fields!");
+    window.alert("Please fill in all the fields!");
+  }
+
+  console.log({ email, password });
+  form.reset();
+}
