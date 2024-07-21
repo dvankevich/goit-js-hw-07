9e3b5d89ddd48b726d08a41c9c47282e92ccbb9a
dvankevich <dvankevich@gmail.com>
2024-07-21 12:02:04 +0300

    create functions

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+//console.log(boxes.innerHTML);
+
+// let boxHtml = `<div style="width: 30px; height: 30px; background-color: blue;"></div>`;
+
+function boxHtml(n, col) {
+  const wh = 30 + 10 * (n - 1);
+  return `<div style="width: ${wh}px; height: ${wh}px; background-color: ${col};"></div>`;
+}
+
+//console.log(boxHtml(2, getRandomHexColor()));
+//boxes.insertAdjacentHTML("beforeend", boxHtml(3, getRandomHexColor()));
+
+function createBoxes(amount) {
+  for (let index = 1; index <= amount; index++) {
+    boxes.insertAdjacentHTML("afterbegin", boxHtml(index, getRandomHexColor()));
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBoxes(3);
+//destroyBoxes();
+createBoxes(5);
+createBoxes(2);
