aa86ad4a722dc02d1e0227bfcdb62a89f2cfc266
dvankevich <dvankevich@gmail.com>
2025-09-26 19:30:52 +0300

    task6 fixes

--- js/task-6.js
+++ js/task-6.js
@@ -4,57 +4,38 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const boxes = document.querySelector("#boxes");
-//console.log(boxes.innerHTML);
-
-// let boxHtml = `<div style="width: 30px; height: 30px; background-color: blue;"></div>`;
+const boxes = document.querySelector('#boxes');
 
 function boxHtml(n, col) {
   const wh = 30 + 10 * (n - 1);
   return `<div style="width: ${wh}px; height: ${wh}px; background-color: ${col};"></div>`;
 }
 
-//console.log(boxHtml(2, getRandomHexColor()));
-//boxes.insertAdjacentHTML("beforeend", boxHtml(3, getRandomHexColor()));
-
 function createBoxes(amount) {
-  let htmlCode = "";
+  let htmlCode = '';
   for (let index = 1; index <= amount; index++) {
-    //boxes.insertAdjacentHTML("afterbegin", boxHtml(index, getRandomHexColor()));
     htmlCode += boxHtml(index, getRandomHexColor());
   }
   boxes.innerHTML = htmlCode;
 }
 
 function destroyBoxes() {
-  boxes.innerHTML = "";
-  //console.log("Destroy button pressed");
+  boxes.innerHTML = '';
 }
 
-const controlBox = document.querySelector("#controls");
-const inputBox = document.querySelector("input");
-//console.log(inputBox.value);
-//const controlsButtons = document.querySelectorAll("button");
-//console.log(controlsButtons);
-//const createButton = controlsButtons[0];
-//const destroyButton = controlsButtons[1];
-const createButton = document.querySelector("[data-create]");
-const destroyButton = document.querySelector("[data-destroy]");
-//console.log(createButton);
-//console.log(destroyButton);
+const controlBox = document.querySelector('#controls');
+const inputBox = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
 
-destroyButton.addEventListener("click", destroyBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+createButton.addEventListener('click', createButtonHandler);
 
-createButton.addEventListener("click", createButtonHandler);
-
-function createButtonHandler(event_onclick) {
-  //console.log(inputBox.value);
+function createButtonHandler() {
   if (inputBox.value > 0 && inputBox.value <= 100) {
-    //console.log(`draw ${inputBox.value} divs`);
     createBoxes(inputBox.value);
-    inputBox.value = "";
+    inputBox.value = '1';
   } else {
-    //console.log("Error message");
-    alert("Value must be in range 1..100");
+    alert('Value must be in range 1..100');
   }
 }
