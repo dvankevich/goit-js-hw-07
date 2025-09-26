3adf749a1ac0e39f928250d0a6372279a38b141b
dvankevich <dvankevich@gmail.com>
2025-09-26 18:57:37 +0300

    task3 fixes

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,11 @@
-"use strict";
+'use strict';
 
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
-
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.getElementById("name-output");
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.getElementById('name-output');
 
 function nameInputHandler(e) {
   const outputText = e.target.value.trim();
-  // console.log(e.target.value);
-  nameOutput.textContent = outputText === "" ? "Anonymous" : outputText;
+  nameOutput.textContent = outputText === '' ? 'Anonymous' : outputText;
 }
 
-nameInput.addEventListener("input", nameInputHandler);
+nameInput.addEventListener('input', nameInputHandler);
